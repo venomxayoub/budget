9387b9f57011a1fe5c730713c76113de1d7758bf
venom <venom@dev.local>
2026-06-30 15:05:01 +0100

    Bump budget manager to 1.3.4

--- budget_manager/android/app/build.gradle.kts
+++ budget_manager/android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.example.budget_manager"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 14
-        versionName = "1.3.3"
+        versionCode = 15
+        versionName = "1.3.4"
     }
 
     signingConfigs {
--- budget_manager/lib/widgets/sidebar.dart
+++ budget_manager/lib/widgets/sidebar.dart
@@ -145,7 +145,7 @@ class _UpdateButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    const version = '1.3.3';
+    const version = '1.3.4';
     const downloadUrl =
         'https://github.com/venomxayoub/budget/releases/download/v$version/BudgetManager-v$version.apk';
 
